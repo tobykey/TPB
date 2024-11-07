0a156b7aee013690ffaf15114a371f1840c37917
Toby <123823724+tobykey@users.noreply.github.com>
2024-11-07 22:34:22 +0900

    인하대탈출 VER 0.2.1
    인하대탈출 VER 0.2.1
    ✔개발 및 수정 사항 - 김태균
    - 메인 화면 개발(5호관 복도)
    ✨ 시작, 클릭 효과음이 적용되었습니다.
    
    -Scene 1 개발(5호관 시계탑)
    ✨ 클릭 효과음이 적용되었습니다.
    ✨ 클릭 시 게임이 시작됩니다!
    
    -Scene 2 개발(5호관 복도2)
    ✨ 클릭 효과음이 적용되었습니다.
    ✨ 좌 우 화살표 및 인벤토리 아이콘이 생성되었습니다.
    ✨ 인벤토리 아이콘에 마우스 오버 시 인벤토리 칸이 보이게 됩니다.
    👉  버그로 인해 휠 클릭으로 기자재실로 넘어가도록 구현해놓았습니다.
    
    -Scene 3 개발(기자재실)
    ✨ 클릭 효과음이 적용되었습니다.
    ✨✨ 인벤토리 기능이 구현되었습니다...!
    ✨✨ UV라이트, 열쇠, 배터리 아이템이 구현되었습니다..!!!!
    ✨ 각 아이템 클릭시 인벤토리로 저장되는 기능이 구현되었습니다.
    ✨✨ 인벤토리에 들어간 아이템은 인벤토리 마우스 오버로 확인할 수 있습니다.

--- escape.py
+++ escape.py
@@ -26,6 +26,39 @@ def fetch_items():
     for user in users:
         print(user)
 
+
+# 인벤토리 시스템
+inventory = []
+
+# UV 라이트 아이템 설정
+class Item:
+    def __init__(self, image, x, y, width, height, name):
+        self.image = pygame.image.load(image)
+        self.image = pygame.transform.scale(self.image, (100, 100))  # 아이템 크기 조정
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.name = name
+        self.collected = False  # 획득 여부
+
+    def draw(self, screen):
+        if not self.collected:
+            screen.blit(self.image, (self.x, self.y))
+
+    def is_clicked(self, mouse_pos):
+        if (self.x <= mouse_pos[0] <= self.x + self.width and
+                self.y <= mouse_pos[1] <= self.y + self.height and
+                    pygame.mouse.get_pressed()[0]):
+            return True
+        return False
+
+# UV 라이트 아이템 생성
+uv_light = Item("resource_pack/image/UV라이트.png", 200, 200, 100, 100, "UV Light")
+key = Item("resource_pack/image/열쇠.png", 300,300,100,100,"key")
+battery = Item("resource_pack/image/건전지.png", 500, 400,100,100, "Battery")
+
+items=[uv_light, key, battery]
 fetch_items()
 
 background = pygame.display.set_mode((900,675))
@@ -38,9 +71,19 @@ image_start = pygame.image.load("resource_pack/image/시작하기.png")
 image_option = pygame.image.load("resource_pack/image/설정.png")
 image_exit = pygame.image.load("resource_pack/image/종료.png")
 
+ui_inventory = pygame.image.load("resource_pack/ui/가방.png")
+ui_right = pygame.image.load("resource_pack/ui/오른쪽 화살표.png")
+ui_left = pygame.image.load("resource_pack/ui/왼쪽 화살표.png")
+
+overlay = pygame.Surface((400, 200), pygame.SRCALPHA)
+overlay.fill((0, 0, 0, 102))  # RGBA 값, 알파 204는 약 80% 투명도
+
 image_loading = pygame.image.load("resource_pack/image/불거진 무대.jpg")
 
-image_scene = pygame.image.load("resource_pack/image/5호관 시계탑.jpg")
+image_scene1 = pygame.image.load("resource_pack/image/5호관 시계탑.jpg")
+image_scene2 = pygame.image.load("resource_pack/image/5호관 복도2.jpg")
+image_scene3 = pygame.image.load("resource_pack/image/기자재실.jpg")
+
 
 image_start_mo = pygame.image.load("resource_pack/image/시작하기_mo.png")
 image_option_mo = pygame.image.load("resource_pack/image/설정_mo.png")
@@ -50,9 +93,21 @@ image_start_click = pygame.image.load("resource_pack/image/시작하기_click.pn
 image_option_click = pygame.image.load("resource_pack/image/설정_click.png")
 image_exit_click = pygame.image.load("resource_pack/image/종료_click.png")
 
+# image_UVlight = pygame.image.load("resource_pack/image/UV라이트.png")
+image_battery = pygame.image.load("resource_pack/image/건전지.png")
+image_key = pygame.image.load("resource_pack/image/열쇠.png")
+
+#BGM 로드
 BGM = pygame.mixer.Sound("resource_pack/BGM.mp3")
-size1 = 0.2
-size2 = 0.7
+# 효과음 로드
+open_sound = pygame.mixer.Sound("resource_pack/effect/문여는소리.mp3")  # 사용할 사운드 파일 경로
+click_sound = pygame.mixer.Sound("resource_pack/effect/클릭.mp3")
+knock_sound = pygame.mixer.Sound("resource_pack/effect/노크.mp3")
+
+size_1 = 0.1
+size_2 = 0.2
+size_5 = 0.5
+size_7 = 0.7
 
 size_bg_width = background.get_size()[0]
 size_bg_height = background.get_size()[1]
@@ -60,35 +115,48 @@ size_bg_height = background.get_size()[1]
 size_loading_width = image_loading.get_size()[0]
 size_loading_height = image_loading.get_size()[1]
 
-size_scene_width = image_scene.get_size()[0]
-size_scene_height = image_scene.get_size()[1]
+size_scene1_width = image_scene1.get_size()[0]
+size_scene1_height = image_scene1.get_size()[1]
 
-size_logo_width = image_logo.get_size()[0]*size1
-size_logo_height = image_logo.get_size()[1]*size1
+size_scene2_width = image_scene2.get_size()[0]
+size_scene2_height = image_scene2.get_size()[1]
 
-size_team_width = image_team.get_size()[0]*size2
-size_team_height = image_team.get_size()[1]*size2
+size_scene3_width = image_scene3.get_size()[0]
+size_scene3_height = image_scene3.get_size()[1]
 
-size_start_width = image_start.get_size()[0]*size1
-size_start_height = image_start.get_size()[1]*size1
-size_start_mo_width = image_start_mo.get_size()[0]*size1
-size_start_mo_height = image_start_mo.get_size()[1]*size1
-size_start_click_width = image_start_click.get_size()[0]*size1
-size_start_click_height = image_start_click.get_size()[1]*size1
+size_logo_width = image_logo.get_size()[0] * size_2
+size_logo_height = image_logo.get_size()[1] * size_2
 
-size_option_width = image_option.get_size()[0]*size1
-size_option_height = image_option.get_size()[1]*size1
-size_option_mo_width = image_option_mo.get_size()[0]*size1
-size_option_mo_height = image_option_mo.get_size()[1]*size1
-size_option_click_width = image_option_click.get_size()[0]*size1
-size_option_click_height = image_option_click.get_size()[1]*size1
+size_team_width = image_team.get_size()[0] * size_7
+size_team_height = image_team.get_size()[1] * size_7
 
-size_exit_width = image_exit.get_size()[0]*size1
-size_exit_height = image_exit.get_size()[1]*size1
-size_exit_mo_width = image_exit_mo.get_size()[0]*size1
-size_exit_mo_height = image_exit_mo.get_size()[1]*size1
-size_exit_click_width = image_exit_click.get_size()[0]*size1
-size_exit_click_height = image_exit_click.get_size()[1]*size1
+size_start_width = image_start.get_size()[0] * size_2
+size_start_height = image_start.get_size()[1] * size_2
+size_start_mo_width = image_start_mo.get_size()[0] * size_2
+size_start_mo_height = image_start_mo.get_size()[1] * size_2
+size_start_click_width = image_start_click.get_size()[0] * size_2
+size_start_click_height = image_start_click.get_size()[1] * size_2
+
+size_option_width = image_option.get_size()[0] * size_2
+size_option_height = image_option.get_size()[1] * size_2
+size_option_mo_width = image_option_mo.get_size()[0] * size_2
+size_option_mo_height = image_option_mo.get_size()[1] * size_2
+size_option_click_width = image_option_click.get_size()[0] * size_2
+size_option_click_height = image_option_click.get_size()[1] * size_2
+
+size_exit_width = image_exit.get_size()[0] * size_2
+size_exit_height = image_exit.get_size()[1] * size_2
+size_exit_mo_width = image_exit_mo.get_size()[0] * size_2
+size_exit_mo_height = image_exit_mo.get_size()[1] * size_2
+size_exit_click_width = image_exit_click.get_size()[0] * size_2
+size_exit_click_height = image_exit_click.get_size()[1] * size_2
+
+size_inventory_width = ui_inventory.get_size()[0]*size_2
+size_inventory_height = ui_inventory.get_size()[1]*size_2
+size_left_width = ui_right.get_size()[0]*size_2
+size_left_height = ui_right.get_size()[1]*size_2
+size_right_width = ui_left.get_size()[0]*size_2
+size_right_height = ui_left.get_size()[1]*size_2
 
 image_logo = pygame.transform.scale(image_logo,(int(size_logo_width),int(size_logo_height)))
 image_team = pygame.transform.scale(image_team,(int(size_team_width),int(size_team_height)))
@@ -103,6 +171,10 @@ image_exit_mo = pygame.transform.scale(image_exit_mo,(int(size_exit_mo_width),in
 image_start_click = pygame.transform.scale(image_start_click,(int(size_start_click_width),int(size_start_click_height)))
 image_option_click = pygame.transform.scale(image_option_click,(int(size_option_click_width),int(size_option_click_height)))
 image_exit_click = pygame.transform.scale(image_exit_click,(int(size_exit_click_width),int(size_exit_click_height)))
+
+ui_inventory = pygame.transform.scale(ui_inventory,(int(size_inventory_width),int(size_inventory_height)))
+ui_right = pygame.transform.scale(ui_right,(int(size_right_width),int(size_right_height)))
+ui_left = pygame.transform.scale(ui_left,(int(size_left_width),int(size_left_height)))
 
 x_pos_logo = 204
 y_pos_logo = 26
@@ -137,6 +209,15 @@ y_pos_exit_mo = 499
 x_pos_exit_click = 400
 y_pos_exit_click = 499
 
+x_pos_left = size_bg_width/2-size_right_width*4
+y_pos_left = size_bg_height/2 - size_right_height/2
+
+x_pos_right = size_bg_width - size_left_width*1.25
+y_pos_right = size_bg_height/2 - size_left_height/2
+
+x_pos_inventory = size_bg_width - size_inventory_width*1.15
+y_pos_inventory = size_bg_height/5 - size_inventory_height
+
 # print(x_pos_logo, y_pos_logo)
 # print(x_pos_team, y_pos_team)
 # print(x_pos_start, y_pos_start)
@@ -164,7 +245,7 @@ while play:
         if event.type == pygame.QUIT:
             play = False
     pygame.mouse.set_cursor(*pygame.cursors.diamond)
-
+    mouse_pos = pygame.mouse.get_pos()
     # 마우스 위치와 클릭 상태 확인
     mouse_x, mouse_y = pygame.mouse.get_pos()
     mouse_buttons = pygame.mouse.get_pressed()
@@ -179,6 +260,7 @@ while play:
             # 클릭 했을 때
             if mouse_buttons[0]:
                 background.blit(image_start_click, (x_pos_start_click, y_pos_start_click))
+                open_sound.play()
                 # time.sleep(3)
                 current_screen += 1
                 loading_start_time = time.time()  # 로딩 타이머 시작
@@ -194,6 +276,7 @@ while play:
                 y_pos_option <= mouse_y <= y_pos_option + size_option_height):
             if mouse_buttons[0]:
                 background.blit(image_option_click, (x_pos_option_click, y_pos_option_click))
+                click_sound.play()  # 효과음 재생
             else:
                 background.blit(image_option_mo, (x_pos_option_mo, y_pos_option_mo))
         else:
@@ -203,6 +286,7 @@ while play:
                 y_pos_exit <= mouse_y <= y_pos_exit + size_exit_height):
             if mouse_buttons[0]:
                 background.blit(image_exit_click, (x_pos_exit_click, y_pos_exit_click))
+                click_sound.play()  # 효과음 재생
                 quitgame()
             else:
                 background.blit(image_exit_mo, (x_pos_exit_mo, y_pos_exit_mo))
@@ -220,7 +304,7 @@ while play:
             current_screen = 3
 
     elif current_screen == 3:
-        background.blit(image_scene,(0,0))
+        background.blit(image_scene1, (0, 0))
         font = pygame.font.Font("resource_pack/font/이순신Bold.ttf", 40)
         text = font.render("이건 인하대학교 5호관에 관한 이야기이다..", True, (255, 255, 255))
         text.set_alpha(alpha)
@@ -231,6 +315,48 @@ while play:
                 fade_in = False
         else:
             alpha -= 0.25
+        text1=font.render("아무 곳이나 클릭하십시오.",True,(255,255,255))
+        background.blit(text1,(250,600))
+        if time.time() - loading_start_time >= 3:  # 3초 로딩
+            if mouse_buttons[0]:
+                click_sound.play()
+                current_screen = 4
 
+    elif current_screen == 4:
+        background.blit(image_scene2, (0, 0))
+        background.blit(ui_inventory,(x_pos_inventory,y_pos_inventory))
+        if (x_pos_inventory <= mouse_x <= x_pos_inventory + size_inventory_width and
+                y_pos_inventory <= mouse_y <= y_pos_inventory + size_inventory_height):
+            background.blit(overlay, (450,100))
+        background.blit(ui_right,(x_pos_right,y_pos_right))
+        background.blit(ui_left,(x_pos_left,y_pos_left))
+
+        if mouse_buttons[1]:
+            click_sound.play()
+            current_screen = 5
+
+    elif current_screen == 5:
+        background.blit(image_scene3, (0, 0))
+        background.blit(ui_inventory, (x_pos_inventory, y_pos_inventory))
+
+        for item in items:
+            if item.is_clicked(mouse_pos) and not item.collected:
+                item.collected = True
+                inventory.append(item)
+                print(f"{item.name} added to inventory.")
+
+        # 아이템 그리기
+        for item in items:
+            item.draw(background)
+
+        if (x_pos_inventory <= mouse_x <= x_pos_inventory + size_inventory_width and
+                y_pos_inventory <= mouse_y <= y_pos_inventory + size_inventory_height):
+            background.blit(overlay, (450,100))
+            # 인벤토리에 아이템 그리기
+            for i, item in enumerate(inventory):
+                font = pygame.font.Font(None, 36)
+                # text = font.render(item, True, (255, 255, 255))
+                # background.blit(text, (450, 120))  # 인벤토리 위치 설정
+                background.blit(item.image, (450 + i * 100, 100))  # 인벤토리 위치를 지정, 간격 조정
     pygame.display.flip()
     
